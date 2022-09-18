4719462dbc4893eed446e88acbafe0a4b9c69fee
losullil <86625869+losullil@users.noreply.github.com>
2022-09-17 23:01:40 -0400

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -1,4 +1,4 @@
 import deeplabcut
 
-config_path = '/mnt/research/jhoneycu/losulliv/KAT_OFT_R/config.yaml'
+config_path = '/mnt/research/jhoneycu/losulliv/KAT_OFT_R/KAT_OFT_R-lucyosullivan-09-17-2022/config.yaml'
 deeplabcut.train_network(config_path, shuffle=1, saveiters=5000, maxiters=200000)
